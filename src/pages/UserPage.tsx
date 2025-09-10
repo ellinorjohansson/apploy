ee85a19623190ffd275980cfdddb1ded0e8295d0
Ellinor Johansson <ellinor.johansson@medieinstitutet.se>
2025-09-10 19:09:23 +0200

    ✨feature: add digitypo and todo list for userPage

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,10 +1,38 @@
 // UserPage.tsx
 
+import { DigiTypography } from '@digi/arbetsformedlingen-react';
+import { TypographyVariation } from '@digi/arbetsformedlingen';
+
+
+
+// TA BORT
+
+// När man klickar på spara knappen i detailpage så ska det id läggas till i denna filen
+// Klickar man i checkboxen ska jobbet flyttas ned till "sökta jobb" och utgråas
+// Detta ska sparas i localhost
+// Lägg till datum som man klickar i checkboxen för att spara när det var man sökte arbetet
+// Fallback om någon av listorna är tomma
+// Ta bort sparade arbeten
+
+// Sparade jobb
+// Sökta jobb
+
+
+
+
 export const UserPage = () => {
   return (
-    <section>
-      <h1>Användarsida</h1>
-      <p>Här kan du se dina sparade annonser m.m.</p>
-    </section>
+    <>
+      <section>
+        <DigiTypography afVariation={TypographyVariation.SMALL}>
+          <h2>Sparade jobb</h2>
+        </DigiTypography>
+      </section>
+      <section>
+        <DigiTypography afVariation={TypographyVariation.SMALL}>
+          <h2>Sökta jobb</h2>
+        </DigiTypography>
+      </section>
+    </>
   );
 };

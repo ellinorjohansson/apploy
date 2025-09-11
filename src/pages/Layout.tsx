ff31c15273f7de8c7015a56173da568c169c038e
Ellinor Johansson <ellinor.johansson@medieinstitutet.se>
2025-09-11 09:36:30 +0200

    ✨feature: add provider in layout so pages can use context savejob

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -3,22 +3,28 @@ import { Header } from "../components/Header"
 import { Footer } from "../components/Footer"
 import { DigiTypography } from "@digi/arbetsformedlingen-react";
 import { TypographyVariation } from "@digi/arbetsformedlingen";
+import { useReducer } from "react";
+import { JobReducer } from "../reducers/SaveJobReducer";
+import { JobsContext } from "../contexts/SaveJobContext";
 
 export const Layout = () => {
-    return (
-    <>
-        <DigiTypography
-            afVariation={TypographyVariation.SMALL}
-        >
-            <header>
-                <Header/>
-            </header>
-            <main>
-                <Outlet/>
-            </main>
-            <footer>
-                <Footer/>
-                </footer>
-        </DigiTypography>
-    </>)
+    const [jobs, dispatch] = useReducer(JobReducer, []);
+
+    return (  
+        <JobsContext.Provider value={{ jobs, dispatch }}>
+            <DigiTypography
+                afVariation={TypographyVariation.SMALL}
+            >
+                <header>
+                    <Header/>
+                </header>
+                <main>
+                    <Outlet/>
+                </main>
+                <footer>
+                    <Footer/>
+                    </footer>
+            </DigiTypography>
+        </JobsContext.Provider>
+    )
 }

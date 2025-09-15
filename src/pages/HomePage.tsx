84f8772ae6401ffc61c6eb17926833d6ccc98c10
Ellinor Johansson <ellinor.johansson@medieinstitutet.se>
2025-09-15 12:28:43 +0200

    ✨feature: add text and fixed columns in homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,51 +1,81 @@
 // HomePage.tsx
 
-import { LayoutBlockVariation } from "@digi/arbetsformedlingen";
-import { DigiLayoutBlock, DigiMediaImage, DigiLayoutContainer, DigiIconChevronDown } from "@digi/arbetsformedlingen-react/src/lib/stencil-generated/components";
-import { useNavigate } from "react-router-dom";
-import { AppButton } from "../components/buttons/AppButton";
-import { Chart } from "../components/Chart";
+import {
+  LayoutBlockVariation,
+  LayoutColumnsElement,
+  LayoutColumnsVariation,
+  TypographyVariation,
+} from '@digi/arbetsformedlingen';
+import {
+  DigiLayoutBlock,
+  DigiMediaImage,
+  DigiLayoutContainer,
+  DigiIconChevronDown,
+} from '@digi/arbetsformedlingen-react/src/lib/stencil-generated/components';
+import { useNavigate } from 'react-router-dom';
+import { AppButton } from '../components/buttons/AppButton';
+import { Chart } from '../components/Chart';
 
-
-import "../components/css/Homepage.css"
+import '../components/css/Homepage.css';
+import { DigiLayoutColumns } from '@digi/arbetsformedlingen-react/src/lib/stencil-generated/components';
+import { DigiTypography } from '@digi/arbetsformedlingen-react/src/lib/stencil-generated/components';
 export const HomePage = () => {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleClick = () => {
-    navigate("/jobs")
-  }
-  
+    navigate('/jobs');
+  };
+
   return (
     <>
       <div className="body-wrapper">
         <DigiLayoutBlock afVariation={LayoutBlockVariation.SECONDARY}>
           <div className="intro-wrapper">
-            <DigiLayoutContainer 
-              afVerticalPadding
-              afNoGutter>
+            <DigiLayoutContainer afVerticalPadding afNoGutter>
               <h2>Hitta ditt drömjobb</h2>
-              <p>Upptäck tusentals aktuella jobbannonser från hela landet – anpassade efter dina intressen och mål.</p>
-              <p>Oavsett om du är ny i arbetslivet eller vill ta nästa steg i karriären finns möjligheten här. Din framtid börjar idag!</p>
+              <p>
+                Upptäck tusentals aktuella jobbannonser från hela landet –
+                anpassade efter dina intressen och mål.
+              </p>
+              <p>
+                Oavsett om du är ny i arbetslivet eller vill ta nästa steg i
+                karriären finns möjligheten här. Din framtid börjar idag!
+              </p>
             </DigiLayoutContainer>
-          <AppButton onClick={handleClick} children={"Hitta nu"}></AppButton>
+            <AppButton onClick={handleClick} children={'Hitta nu'}></AppButton>
           </div>
-            <figure className="hero-container">
-              <DigiMediaImage
-                afUnlazy
-                afFullwidth
-                afSrc='/images/Hero-image AF.png'
-                afAlt="Hero image for Apploys homepage"
-                >
-              </DigiMediaImage>
-              <a className="chevron-container" href="#chart-container">
-                <DigiIconChevronDown></DigiIconChevronDown>
-              </a>
-            </figure>
-            {/* <NavLink to={"/user"}>Till din sida</NavLink> */}
+          <figure className="hero-container">
+            <DigiMediaImage
+              afUnlazy
+              afFullwidth
+              afSrc="/images/Hero-image AF.png"
+              afAlt="Hero image for Apploys homepage"
+            ></DigiMediaImage>
+            <a className="chevron-container" href="#chart-container">
+              <DigiIconChevronDown></DigiIconChevronDown>
+            </a>
+          </figure>
+          {/* <NavLink to={"/user"}>Till din sida</NavLink> */}
         </DigiLayoutBlock>
         <div id="chart-container">
-          <Chart></Chart>
+          <DigiLayoutBlock>
+            <DigiLayoutColumns
+              afElement={LayoutColumnsElement.DIV}
+              afVariation={LayoutColumnsVariation.TWO}
+            >
+              <div>
+                <DigiTypography
+                  afVariation={TypographyVariation.SMALL}
+                >
+                  <h2>Diagram</h2>
+                  <p>blablabla</p>
+                </DigiTypography>
+              </div>
+              <div>
+                <Chart></Chart>
+              </div>
+            </DigiLayoutColumns>
+          </DigiLayoutBlock>
         </div>
       </div>
     </>
